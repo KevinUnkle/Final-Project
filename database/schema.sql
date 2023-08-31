1abfebaa6783c924e354d2932a86514df88cd0bd
Kevin Unkle <kevinunk@gmail.com>
2023-08-31 18:36:01 +0000

    Completed deployment prep

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,31 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+CREATE TABLE "public"."users" (
+	"username" serial NOT NULL,
+	"userId" serial NOT NULL,
+	"hashedPassword" TEXT NOT NULL,
+	"createdAt" timestamptz(6) NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."workouts" (
+	"workoutNotes" TEXT NOT NULL,
+	"userId" integer NOT NULL,
+	"workoutId" serial NOT NULL,
+	"muscleGroup" TEXT NOT NULL,
+	"workoutDate" DATE NOT NULL,
+	"createdAt" timestamptz(6) NOT NULL,
+	CONSTRAINT "workouts_pk" PRIMARY KEY ("workoutId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "public"."workouts" ADD CONSTRAINT "workouts_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
